944f425520cab38d823ed4cddb3458ea645e16db
Gaby Mendez <gmendez90@gmail.com>
2020-05-13 11:55:35 -0400

    Complete iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -8,5 +8,28 @@ class House
 
   def add_room(room)
     @rooms << room
-  end 
+  end
+
+  def above_market_average?
+    true if @price > 500000
+  end
+
+  def rooms_from_category(category)
+    room_category_hash =
+    @rooms.group_by do |room|
+      room.category
+    end
+    room_category_hash[category]
+  end
+
+  def area
+    @rooms.reduce(0) do |sum, room|
+      sum += room.area
+    end
+  end
+
+  def details
+    Hash.new.merge("price" => @price, "address" => @address)
+  end
+
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -6,6 +6,10 @@ require './lib/house'
 class HouseTest < Minitest::Test
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
   end
 
   def test_it_exists
@@ -25,12 +29,40 @@ class HouseTest < Minitest::Test
   end
 
   def test_it_can_add_new_rooms
-    room_1 = Room.new(:bedroom, 10, '13')
-    room_2 = Room.new(:bedroom, 11, '15')
-    @house.add_room(room_1)
-    @house.add_room(room_2)
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
 
-    assert_equal [room_1, room_2], @house.rooms
+    assert_equal [@room_1, @room_2], @house.rooms
+  end
+
+  def test_if_above_market_average
+    refute @house.above_market_average?
+  end
+
+  def test_categorize_rooms
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+  end
+
+  def test_it_has_total_area
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+
+    assert_equal 1900, @house.area
+  end
+
+  def test_it_has_details
+    assert_instance_of Hash, @house.details
+    assert_equal 400000, @house.details["price"]
+    assert_equal "123 sugar lane", @house.details["address"]
   end
 
 end
